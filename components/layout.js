075d88bd7208ae6b4071d4d7ad2bf6a40193bfe5
Jacob Quigley <quigley.jacob3@icloud.com>
2021-01-02 00:33:32 -0600

    Testing Branch

--- components/layout.js
+++ components/layout.js
@@ -3,8 +3,8 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'General Kenobi'
-const profile = 'kenobi'
+const name = 'Grand Admiral Thrawn'
+const profile = 'thrawn'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
